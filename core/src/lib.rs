2bbd8d6727b1e5f342bbf6e38e30a02352f8a462
Daniel Boros <dancixx@gmail.com>
2025-05-24 22:30:40 +0200

    feat: add struct methods

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,6 @@
-use darling::{Error, ast};
+use darling::{Error, FromDeriveInput, FromField, ast};
 use proc_macro::TokenStream;
+use quote::ToTokens;
 use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(FromRow)]
@@ -12,10 +13,81 @@ pub fn derive_from_row(input: TokenStream) -> TokenStream {
     }
 }
 
-fn try_derive_from_row(input: &DeriveInput) -> Result<TokenStream, Error> {}
+fn try_derive_from_row(input: &DeriveInput) -> Result<TokenStream, Error> {
+    let from_row_derive = DeriveFromRow::from_derive_input(input)?;
+    Ok(from_row_derive.generate()?)
+}
 
+#[derive(Debug, FromDeriveInput)]
+#[darling(
+    // attributes(from_row),
+    forward_attrs(allow, doc, cfg),
+    supports(struct_named)
+)]
 struct DeriveFromRow {
     ident: syn::Ident,
+    vis: syn::Visibility,
     generics: syn::Generics,
-    data: ast::Data<(), ()>,
+    data: ast::Data<(), FromRowField>,
+    attrs: Vec<syn::Attribute>,
+}
+
+impl DeriveFromRow {
+    fn generate(&self) -> syn::Result<TokenStream> {
+        // get fields
+        let fields = match &self.data {
+            ast::Data::Struct(fields) => fields.fields.as_slice(),
+            _ => unimplemented!(),
+        };
+
+        // validate fields
+        for f in fields {
+            f.validate()?;
+        }
+
+        Ok(TokenStream::new())
+    }
+}
+
+#[derive(Debug, FromField)]
+#[darling(attributes(from_row), forward_attrs(allow, doc, cfg))]
+struct FromRowField {
+    ident: Option<syn::Ident>,
+    vis: syn::Visibility,
+    ty: syn::Type,
+    from: Option<String>,
+    try_from: Option<String>,
+    raname: Option<String>,
+}
+
+impl FromRowField {
+    /// Validate all rules on field level
+    fn validate(&self) -> syn::Result<()> {
+        if self.from.is_some() && self.try_from.is_some() {
+            return Err(Error::custom(r#"cannot specify both `from` and `try_from`"#).into());
+        }
+
+        Ok(())
+    }
+
+    /// Get the target field type
+    fn target_field_ty(&self) -> syn::Result<proc_macro2::TokenStream> {
+        if let Some(from) = &self.from {
+            return Ok(from.parse()?);
+        }
+
+        if let Some(try_from) = &self.try_from {
+            return Ok(try_from.parse()?);
+        }
+
+        Ok(self.ty.to_token_stream())
+    }
+
+    /// Handle renaming of fields
+    fn target_field_name(&self) -> String {
+        self.raname
+            .as_ref()
+            .map(Clone::clone)
+            .unwrap_or(self.ident.as_ref().unwrap().to_string())
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@ pub use tokio_postgres_fromrow_core::FromRow;
 
 pub trait FromRow: Sized {
     fn from_row(row: &tokio_postgres::Row) -> Self;
+    fn try_from_row(row: &tokio_postgres::Row) -> Result<Self, tokio_postgres::Error>;
 }
